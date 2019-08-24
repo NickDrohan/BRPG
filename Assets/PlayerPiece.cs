f569717d4d777e482a7b7b0bcebdec82a037801e
NickDrohan <nickdrohan@gmail.com>
2019-08-23 21:12:25 -0400

    Add files via upload

---
+++ Assets/PlayerPiece.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPiece : GamePiece
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            moveRight();
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            moveLeft();
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            moveDown();
+        }
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            moveUp();
+        }
+        if (Input.GetKeyDown(KeyCode.E) || Input.mouseScrollDelta.y > 0)
+        {
+            rightate();
+        }
+        if (Input.GetKeyDown(KeyCode.Q) || Input.mouseScrollDelta.y < 0)
+        {
+            leftate();
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            dropPiece();
+        }
+    }
+}
---
+++ Assets/boardController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boardController : MonoBehaviour
+{
+    public GameObject newPiece;
+    public bool canPlace = true;
+    public void addGamePiece(GameObject newObject)
+    {
+        if (canPlace)
+        {
+            canPlace = false;
+            newPiece = Instantiate(newObject);
+            newPiece.transform.SetParent(transform);
+            newPiece.transform.localPosition = new Vector3(0.0f, 0.5f, 0.0f);
+            newPiece.AddComponent<PlayerPiece>();
+            StartCoroutine(repeatUntilPieceIsPlaced());
+        }
+    }
+
+    IEnumerator repeatUntilPieceIsPlaced()
+    {
+        while (true)
+        {
+            if (newPiece.GetComponent<PlayerPiece>() == null)
+            {
+                canPlace = true;
+                break;
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+    
+}
